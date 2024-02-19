c49c7a3a7a7430b7f81b2446440e9128be37ca43
rafifazzaki <rafifazzaki@ymail.com>
2024-02-19 15:00:55 +0700

    Initial commit

---
+++ projectA/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+class Program {
+    static void Main(){
+        Car avanza = new Car();
+        avanza.name = "avanza";
+        avanza.brand = "toyota";
+        avanza.type = "wagon";
+        avanza.year = 2018;
+        Console.WriteLine(avanza.brand);
+        avanza.Drive();
+
+        Car ignis = new Car();
+        ignis.name = "ignis";
+        ignis.type = "wagon";
+        ignis.brand = "suzuki";
+        ignis.year = 2024;
+        Console.WriteLine(ignis.brand);
+        ignis.Brake();
+    }
+}
+
+class Car {
+    public string name;
+    public string brand;
+    public string type;
+    public string colour;
+    public int year;
+    public bool isStillOnProduction = true;
+
+    public void Drive(){
+        Console.WriteLine("drive");
+    }
+
+    public void Turn(){
+        Console.WriteLine("Turn");
+    }
+
+    public void Brake(){
+        Console.WriteLine("brake");
+    }
+}
---
+++ solution/projectC/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+class Program{
+    static void Main(){
+        Calculator calc = new Calculator();
+        int result = calc.Addition(3,2); //camelcase
+        Console.WriteLine($"Hasil Addition: {result}");
+        Console.WriteLine(calc.Substraction(3,2));
+        Console.WriteLine(calc.Multiplication(3,2));
+        Console.WriteLine(calc.Division(3,2));
+    }
+}
+
+
+class Calculator{ //PascalCase
+    public int Addition(int number1, int number2){
+        return number1+number2;
+    }
+
+    public int Substraction(int number1, int number2){
+        return number1-number2;
+    }
+
+    public int Multiplication(int number1, int number2){
+        return number1*number2;
+    }
+
+    public int Division(int number1, int number2){
+        return number1/number2;
+    }
+}
