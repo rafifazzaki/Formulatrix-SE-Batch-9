5543d2ed6f75fd72f8161decc8586db3f1514605
rafifazzaki <59862154+rafifazzaki@users.noreply.github.com>
2024-03-05 14:58:29 +0700

    week 3 day 2

---
+++ Finalizer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+// GC = Garbage Collector
+class Program{
+    static void Main(){
+        int iteration = 1000;
+        Stopwatch stopwatch = new();
+        stopwatch.Start();
+        for(int i = 0; i < iteration; i++) {
+            InstanceCreator();
+        }
+        stopwatch.Stop();
+
+        // maksa buat manggil GC
+        // GC.Collect(0); //klo ngga salah bisa kyk gini
+        GC.Collect(); //jangan make ini: bakal ngebersihin dan ngefreeze programnya sebentar
+        GC.Collect();
+        GC.WaitForPendingFinalizers(); //nunggu finalizernya dijalanin dulu baru lanjut lagi
+
+        Console.WriteLine("Time Elapsed: ");
+        Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+
+        // GC ada multithread khusus, dan pas Finalizer jg ada thread sendiri lagi
+    }
+    static void InstanceCreator() {
+        Human human = new Human();
+    }
+}
+
+class Human{
+    // jangan membuat Destructor walaupun kosongan seperti ini, karena tetap akan dipanggil:
+    // ~Human(){ }
+    public Human() { //Constructor
+        Console.WriteLine("Human created");
+        Console.WriteLine(GC.GetGeneration(this));
+    }
+
+    // Destructor akan dipanggil pas GC bersih-bersih, jadi kita tidak tau kapan bakal di jalankan
+    // saat GC membersihkan memory dan ketemu dengan deconstructor ini, maka dia akan memindahkannya ke gen selanjutnya
+    // dan baru akan dijalankan saat gen selanjutnya
+    ~Human() { //Destructor
+        Console.WriteLine("Human destroyed");
+        Console.WriteLine(GC.GetGeneration(this));
+    }
+}
